1c3cade480ba8e092fd6a0d7728ec7c414a4ca58
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-11-05 17:13:16 +0530

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -1524,25 +1524,31 @@ async def _change_metadata(event):
                 link = await get_url_from_message(new_event)
             else:
                 return
-        metadata_event = await ask_text_event(chat_id, user_id, event, 120, "Send MetaData", message_hint="🔷`a` Is For Audio & `s` Is For Subtitle\n🔷 Send In The Format As Shown Below:\n\n`a:0-AudioLanguage-AudioTitle` (To Change Audio Number 1 Metadata)\n`s:0-SubLanguage-SubTitle` (To Change Subtitle Number 1 Metadata)\n\ne.g. `a:1-eng-nik66bots` (To Change Audio Number 2 Metadata)")
+        metadata_event = await ask_text_event(chat_id, user_id, event, 120, "Send Metadata", message_hint="🔷`a` Is For Audio & `s` Is For Subtitle\n🔷 Send In The Format As Shown Below:\n\n`a:0-AudioLanguage-AudioTitle` (To Change Audio Number 1 Metadata)\n`s:0-SubLanguage-SubTitle` (To Change Subtitle Number 1 Metadata)\n\n`v:VideoTitle` (To Change Video Title)\n\nFor example: `v:MyAwesomeVideo`")
         if not metadata_event:
             return
         custom_metadata_list = str(metadata_event.message.message).split('\n')
         custom_metadata = []
+        video_title = ""
+
         for m in custom_metadata_list:
             mdata = str(m).strip().split('-')
             LOGGER.info(mdata)
-            try:
-                sindex = str(mdata[0]).strip().lower()
-                mlang =  str(mdata[1]).lower()
-                mtilte = str(mdata[2])
-                custom_metadata.append([f'-metadata:s:{sindex}', f"language={mlang}", f'-metadata:s:{str(sindex)}', f"title={mtilte}"])
-            except Exception as e:
-                await metadata_event.reply(f"❗Invalid Metadata, Error: {str(e)}")
-                return
+
+            if mdata[0].lower().startswith('v'):  # Video title metadata
+                 video_title = str(mdata[0][2:])  # Extract video title
+            else:
+                try:
+                    sindex = str(mdata[0]).strip().lower()
+                    mlang =  str(mdata[1]).lower()
+                    mtilte = str(mdata[2])
+                    custom_metadata.append([f'-metadata:s:{sindex}', f"language={mlang}", f'-metadata:s:{str(sindex)}', f"title={mtilte}"])
+                except Exception as e:
+                    await metadata_event.reply(f"❗Invalid Metadata, Error: {str(e)}")
+                    return
         user_name = get_username(event)
         user_first_name = event.message.sender.first_name
-        process_status = ProcessStatus(user_id, chat_id, user_name, user_first_name, event, Names.changeMetadata, custom_file_name, custom_metadata=custom_metadata)
+        process_status = ProcessStatus(user_id, chat_id, user_name, user_first_name, event, Names.changeMetadata, custom_file_name, custom_metadata=custom_metadata, video_title=video_title)
         task = {}
         task['process_status'] = process_status
         task['functions'] = []
@@ -1554,7 +1560,6 @@ async def _change_metadata(event):
         create_task(add_task(task))
         await update_status_message(event)
         return
-
 
 ###############------Change_index------###############
 @TELETHON_CLIENT.on(events.NewMessage(incoming=True, pattern='/changeindex', func=lambda e: user_auth_checker(e)))
